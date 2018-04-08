9bb31b1d7f0962d07c76ffad486f2429c4763bb7
yashjain12yj <yashjain12yj@gmail.com>
2018-04-08 22:41:23 +0530

    first commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { getFunName } from '../helpers'
+
+
+class StorePicker extends React.Component{
+	// constructor(){
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+
+	goToStore(event){
+		event.preventDefault();
+		// grap the text
+		const value = this.storeInput.value;
+		console.log(value);
+		// redirect
+		this.context.router.transitionTo(`/store/${value}`);
+	}
+	render(){
+		return(
+			<form 
+				className="store-selector" 
+				onSubmit={(e) => this.goToStore(e)} >
+				{/* form for store selector */}
+				<h2>Please Enter A Store</h2>
+				<input 
+					type="text" 
+					required 
+					placeholder="Store Name"
+					defaultValue={getFunName()}
+					ref={(input)=> { this.storeInput = input}}
+				/>
+				<button type="submit">Visit Store</button>
+			</form>
+		); 
+	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
+}
+
+export default StorePicker;
