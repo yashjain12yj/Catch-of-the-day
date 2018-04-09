b0aa29a729e7a62b2a2c1f8744e6c87a370b4680
yashjain12yj <yashjain12yj@gmail.com>
2018-04-09 23:45:29 +0530

    displaying order state with jsx

--- src/components/Order.js
+++ src/components/Order.js
@@ -6,6 +6,7 @@ class Order extends React.Component{
 		super();
 		this.renderOrder = this.renderOrder.bind(this);
 	}
+	
 	renderOrder(key){
 		const fish = this.props.fishes[key];
 		const count = this.props.order[key];
